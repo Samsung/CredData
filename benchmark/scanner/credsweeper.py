271ccf36c820431cd0c8b65b29a321a72ece77a7
Roman Babenko <babenek@gmail.com>
2023-06-27 21:25:55 +0300

    apply --sort for result

--- benchmark/scanner/credsweeper.py
+++ benchmark/scanner/credsweeper.py
@@ -29,7 +29,7 @@ class CredSweeper(Scanner):
         self.init_scanner()
         subprocess.call([
             "./venv/bin/python", "-m", "credsweeper", "--banner", "--path", f"{self.cred_data_dir}/data", "--jobs", "4",
-            "--save-json", self.output_dir
+            "--save-json", self.output_dir, "--sort"
         ],
                         cwd=self.scanner_dir)
 
