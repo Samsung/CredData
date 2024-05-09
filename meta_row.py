794f1d1c7da618caa924f1a11a12c1a45ca152e8
Roman Babenko <babenek@gmail.com>
2024-05-09 15:17:04 +0300

    review data refactoring

--- meta_row.py
+++ meta_row.py
@@ -1,7 +1,13 @@
+import csv
+import os
+from pathlib import Path
+from typing import Union, List, Generator
+
+
 class MetaRow:
     """Class represented meta markup row structure"""
 
-    Id: str
+    Id: int
     FileID: str
     Domain: str
     RepoName: str
@@ -26,18 +32,87 @@ class MetaRow:
     Category: str
 
     def __init__(self, row: dict):
+        if not isinstance(row, dict):
+            raise RuntimeError(f"ERROR: wrong row {row}")
         for key, typ in self.__annotations__.items():
+            val = None
             if key.startswith("__"):
                 continue
             row_val = row.get(key)
             if row_val is not None:
-                if (typ is int or typ is float) and row_val:
-                    val = typ(row_val)
+                if typ is int:
+                    if row_val:
+                        val = typ(row_val)
+                    else:
+                        val = -1
+                elif typ is float:
+                    if row_val:
+                        val = typ(row_val)
+                    else:
+                        val = 0.0
                 elif typ is str and isinstance(row_val, str):
                     val = row_val
                 self.__setattr__(key, val)
+        if self.LineStart > self.LineEnd:
+            raise RuntimeError(f"ERROR: LineStart must be lower than LineEnd {row}")
 
     def __str__(self) -> str:
         dict_values = self.__dict__.values()
         _str = ','.join(str(x) for x in dict_values)
         return _str
+
+    def __repr__(self):
+        return str(self)
+
+
+def _meta_from_file(meta_path: Path) -> Generator[dict, None, None]:
+    if ".csv" != meta_path.suffix:
+        # *.csv.orig artifacts after git merge
+        print(f"WARNING: skip {meta_path} file")
+        return
+    with open(meta_path) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            if not isinstance(row, dict):
+                raise RuntimeError(f"ERROR: wrong row '{row}' in {meta_path}")
+            yield row
+
+
+def _meta_from_dir(meta_path: Path) -> Generator[dict, None, None]:
+    for root, dirs, files in os.walk(meta_path):
+        root_path = Path(root)
+        for file in files:
+            yield from _meta_from_file(root_path / file)
+        # meta dir is flat
+        break
+
+
+def _get_source_gen(meta_path: Union[Path]) -> Generator[dict, None, None]:
+    if not isinstance(meta_path, Path):
+        raise RuntimeError(f"ERROR: unsupported source {meta_path} type {type(meta_path)}")
+
+    if not meta_path.exists():
+        raise RuntimeError(f"ERROR: {meta_path} does not exist")
+
+    if meta_path.is_dir():
+        source_gen = _meta_from_dir
+    elif meta_path.is_file():
+        source_gen = _meta_from_file
+    else:
+        raise RuntimeError(f"ERROR: unsupported {meta_path} file type")
+    yield from source_gen(meta_path)
+
+
+def read_meta(meta_dir: Union[str, Path]) -> List[MetaRow]:
+    meta = []
+    meta_ids = set()
+
+    for row in _get_source_gen(Path(meta_dir)):
+        meta_row = MetaRow(row)
+        if meta_row.Id in meta_ids:
+            raise RuntimeError(f"ERROR: duplicate Id row {row}")
+        meta_ids.add(meta_row.Id)
+
+        meta.append(meta_row)
+
+    return meta
--- review_data.py
+++ review_data.py
@@ -20,6 +20,7 @@ from typing import Dict, List, Optional
 from colorama import Fore, Back, Style
 
 from meta_cred import MetaCred
+from meta_row import read_meta
 
 
 def read_data(path, line_start, line_end, value_start, value_end, ground_truth, creds: List[MetaCred]):
@@ -95,47 +96,6 @@ def read_data(path, line_start, line_end, value_start, value_end, ground_truth, 
     print("\n\n")
 
 
-def read_meta(meta_dir, data_dir) -> List[Dict[str, str]]:
-    meta = []
-    ids = set()
-    id_dups = []
-    for root, dirs, files in os.walk(meta_dir):
-        root_path = Path(root)
-        for file in files:
-            if 12 != len(file) or not all('0' <= x <= '9' or 'a' <= x <= 'f' for x in file[:8]):
-                # git garbage case
-                continue
-            with open(root_path / file, newline="") as f:
-                reader = csv.DictReader(f)
-                for row in reader:
-                    assert 23 == len(row), row
-                    # verify correctness of data
-                    file_path = row["FilePath"]
-                    if file_path.startswith("data/"):
-                        row["FilePath"] = f"{data_dir}/{file_path[5:]}"  # todo: check slicing
-                    elif file_path.startswith("/"):
-                        pass  # keep as is - absolute path
-                    else:
-                        raise RuntimeError(f"Invalid path:", row)
-                    row["LineStart"] = int(row["LineStart"])
-                    row["LineEnd"] = int(row["LineEnd"])
-                    assert row["LineStart"] <= row["LineEnd"], row
-                    value_start = row["ValueStart"]
-                    row["ValueStart"] = int(value_start) if value_start else -1
-                    value_end = row["ValueEnd"]
-                    row["ValueEnd"] = int(value_end) if value_end else -1
-                    assert -1 == row["ValueStart"] or -1 == row["ValueEnd"] or row["ValueStart"] <= row["ValueEnd"], row
-                    meta.append(row)
-                    if row["Id"] in ids:
-                        row_csv = ','.join([str(x) for x in row.values()])
-                        id_dups.append(row_csv)
-                        print(f"Check id duplication: {row_csv}")
-                    else:
-                        ids.add(row["Id"])
-    assert not id_dups, '\n'.join(id_dups)
-    return meta
-
-
 def main(meta_dir, data_dir, data_filter, load_json: Optional[str] = None, category: Optional[str] = None):
     if not os.path.exists(meta_dir):
         raise FileExistsError(f"{meta_dir} directory does not exist.")
@@ -146,31 +106,30 @@ def main(meta_dir, data_dir, data_filter, load_json: Optional[str] = None, categ
         with open(load_json, "r") as f:
             creds.extend([MetaCred(x) for x in json.load(f)])
 
-    meta = read_meta(meta_dir, data_dir)
-    meta.sort(key=lambda x: (x["FilePath"], x["LineStart"], x["LineEnd"], x["ValueStart"], x["ValueEnd"]))
+    meta = read_meta(meta_dir)
+    meta.sort(key=lambda x: (x.FilePath, x.LineStart, x.LineEnd, x.ValueStart, x.ValueEnd))
     displayed_rows = 0
     shown_rows = set()
     for row in meta:
-        if not data_filter[row["GroundTruth"]]:
+        if not data_filter[row.GroundTruth]:
             continue
-        if category and not category == row["Category"]:
+        if category and not category == row.Category:
             continue
 
         displayed_rows += 1
-        row_csv = ','.join([str(x) for x in row.values()])
-        print(row_csv, flush=True)
+        print(str(row), flush=True)
         try:
-            file_path = str(row["FilePath"])
-            line_start = int(row["LineStart"])
-            line_end = int(row["LineEnd"])
-            value_start = int(row["ValueStart"])
-            value_end = int(row["ValueEnd"])
+            file_path = str(row.FilePath)
+            line_start = int(row.LineStart)
+            line_end = int(row.LineEnd)
+            value_start = int(row.ValueStart)
+            value_end = int(row.ValueEnd)
             read_data(file_path,
                       line_start,
                       line_end,
                       value_start,
                       value_end,
-                      row["GroundTruth"],
+                      row.GroundTruth,
                       creds)
         except Exception as exc:
             print(f"Failure {row}", exc, flush=True)
