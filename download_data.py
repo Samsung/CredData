ac9775b685a25e3e22370a6f36b31dcd332d7c1a
Roman Babenko <babenek@gmail.com>
2024-08-06 09:00:50 +0300

    jwt obf

--- download_data.py
+++ download_data.py
@@ -219,8 +219,6 @@ CHARS4RAND = (string.digits + string.ascii_lowercase + string.ascii_uppercase).e
 DIGITS = string.digits.encode("ascii")
 # 0 on first position may break json e.g. "id":123, -> "qa":038, which is incorrect json
 DIGITS4RAND = DIGITS[1:]
-CHARS4OBF = {ord(x) for x in string.ascii_lowercase + string.ascii_uppercase if
-             x not in "falsetrun"}
 
 
 def obfuscate_jwt(value: str) -> str:
@@ -234,20 +232,42 @@ def obfuscate_jwt(value: str) -> str:
         decoded = base64.b64decode(value, validate=True)
     new_json = bytearray(len(decoded))
     backslash = False
-    for n, i in enumerate(decoded):
+    n = 0
+    while len(decoded) > n:
         if backslash:
             new_json[n] = 0x3F  # ord('?')
             backslash = False
+            n += 1
             continue
-        if i in DIGITS:
+        for wrd in [b"null", b"false", b"true",
+                    b"alg", b"typ", b"kid",
+                    b"jku", b"jwk", b"crit", b"iat", b"nbf",
+                    b"x5t#S256", b"x5u", b"x5c", b"x5t",
+                    b"iss", b"exp", b"sub", b"role", b"enc", b"cty",
+                    b"cid", b"jti", b"arn", b"type", b"zip", b"aud",
+                    b"username", b"name",
+                    b"id",
+                    b"JWT", b"HS256", b"none"
+                    ]:
+            # safe words to keep JSON structure (false, true, null)
+            # and important JWT ("alg", "type", ...)
+            if decoded[n:n + len(wrd)] == wrd:
+                end_pos = n + len(wrd)
+                while n < end_pos:
+                    new_json[n] = decoded[n]
+                    n += 1
+                continue
+        # any other data will be obfuscated
+        if decoded[n] in DIGITS:
             new_json[n] = random.choice(DIGITS4RAND)
-        elif i in CHARS4OBF:
+        elif decoded[n] in CHARS4RAND:
             new_json[n] = random.choice(CHARS4RAND)
-        elif '\\' == i:
+        elif '\\' == decoded[n]:
             new_json[n] = 0x3F  # ord('?')
             backslash = True
         else:
-            new_json[n] = i
+            new_json[n] = decoded[n]
+        n += 1
 
     encoded = base64.b64encode(new_json, altchars=b"-_").decode("ascii")
     while len(encoded) > len_value:
