3e745911d89f81e7b69d49f27103169e250ddfec
Roman Babenko <babenek@gmail.com>
2024-12-09 09:53:05 +0200

    [skip actions] [esc] 2024-12-09T09:53:05+02:00

--- benchmark/scanner/scanner.py
+++ benchmark/scanner/scanner.py
@@ -362,7 +362,7 @@ class Scanner(ABC):
                     subprocess.check_call(
                         ["sed", "-i",
                          f"s/{row.Id},\\(.*\\)/{row.Id},\\1:{rule}/",
-                         f"{self.cred_data_dir}/meta/{row.RepoName}"])
+                         f"{self.cred_data_dir}/meta/{row.RepoName}.csv"])
         # meta has no markup for given credential
         self.lost_cnt += 1
         print(f"{suggestion} {approximate}", flush=True)
