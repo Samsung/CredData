8c5e5039f0762f889713bdd417fef64ce0b59b98
Roman Babenko <babenek@gmail.com>
2025-10-25 19:52:01 +0300

    improve markup_report.py script

--- markup_report.py
+++ markup_report.py
@@ -6,8 +6,8 @@ currently the row from meta is placed to "ml_validation" to keep the value at th
 """
 
 import json
+import os
 import sys
-from argparse import ArgumentParser
 from pathlib import Path
 from typing import Dict, List, Tuple
 
@@ -15,10 +15,10 @@ from meta_cred import MetaCred
 from meta_row import MetaRow, _get_source_gen
 
 
-def prepare_meta(meta_dir) -> Dict[Tuple[str, int, int, int, int], List[MetaRow]]:
+def prepare_meta(meta_dir: Path) -> Dict[Tuple[str, int, int, int, int], List[MetaRow]]:
     meta_dict: Dict[Tuple[str, int, int, int, int], List[MetaRow]] = {}
 
-    for row in _get_source_gen(Path(meta_dir)):
+    for row in _get_source_gen(meta_dir):
         meta_row = MetaRow(row)
         markup_key = (meta_row.FilePath, meta_row.LineStart, meta_row.LineEnd, meta_row.ValueStart, meta_row.ValueEnd)
         if meta_list := meta_dict.get(markup_key):
@@ -30,10 +30,10 @@ def prepare_meta(meta_dir) -> Dict[Tuple[str, int, int, int, int], List[MetaRow]
     return meta_dict
 
 
-def main(report_file: str, meta_dir: str):
+def markup(report_file: Path, meta_dir: Path):
     errors = 0
 
-    with open(report_file, "r") as f:
+    with open(report_file) as f:
         creds = json.load(f)
 
     meta_dict = prepare_meta(meta_dir)
@@ -60,19 +60,37 @@ def main(report_file: str, meta_dir: str):
             # something was wrong
             errors += 1
 
-    with open(f"{report_file}", "w") as f:
+    with open(report_file, mode='w') as f:
         json.dump(creds, f, indent=4)
 
     return errors
 
 
+def main(argv) -> int:
+    meta_dir = Path(__file__).parent / "meta"
+    reports = []
+    for n, arg in enumerate(argv[1:]):
+        arg_path = Path(arg)
+        if arg_path.is_dir() and arg_path.exists():
+            print(f"Use specific meta dir: {arg_path}", flush=True)
+            meta_dir = arg_path
+        elif arg_path.is_file() and arg_path.exists():
+            reports.append(arg_path)
+        else:
+            print(f"Unrecognized {n + 1} argument: '{arg}'", flush=True)
+    if not (meta_dir.is_dir() and meta_dir.exists()):
+        print(f"Not exists meta dir: '{meta_dir.absolute()}'", flush=True)
+        return 1
+    error_code = os.EX_OK
+    for report_file in reports:
+        try:
+            if errors := markup(report_file, meta_dir):
+                print(f"{errors} candidates were not found in {meta_dir} markup", flush=True)
+                error_code = 1
+        except Exception as exc:
+            print(f"{report_file} - failure: {exc}")
+            error_code = 1
+    return error_code
+
 if __name__ == "__main__":
-    parser = ArgumentParser(prog="python markup_report.py",
-                            description="Console script for markup report file from credsweeper")
-
-    parser.add_argument("load_report", help="Load json report from CredSweeper")
-    parser.add_argument("meta_dir", help="Markup location", nargs='?', default="meta")
-
-    _args = parser.parse_args()
-    exit_code = main(_args.load_report, _args.meta_dir)
-    sys.exit(exit_code)
+    sys.exit(main(sys.argv))
