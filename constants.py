b1bef855e0f54264c6ae66ecb5b25e57fb072ce6
Roman Babenko <babenek@gmail.com>
2025-12-14 12:04:08 +0200

    Other category is common

--- constants.py
+++ constants.py
@@ -2,5 +2,8 @@ LABEL_TRUE = 'T'
 LABEL_FALSE = 'F'
 LABEL_OTHER = 'X'
 ALLOWED_LABELS = (LABEL_TRUE, LABEL_FALSE, LABEL_OTHER)
+# the category is used to markup undecided values
+OTHER_CATEGORY = "Other"
 PRIVATE_KEY_CATEGORY = "PEM Private Key"
-MULTI_PATTERN_RULES = ("AWS Multi", "Google Multi", "JWK")
+# the rules may have multiline markup and should be skipped for single line obfuscation
+MULTI_PATTERN_RULES = ("AWS Multi", "Google Multi", "JWK", OTHER_CATEGORY, PRIVATE_KEY_CATEGORY)
