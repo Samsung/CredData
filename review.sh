e04763efb45152a54e058bbbfe6a678cd0f59b85
Roman Babenko <babenek@users.noreply.github.com>
2026-07-06 17:44:56 +0300

    codeql (#312)
    * codeql w/o .expected
    
    * empty BM scores fix

--- review.sh
+++ review.sh
@@ -2,12 +2,12 @@
 
 set -ex
 
-.venv/bin/python review_data.py --check
+.venv/bin/python3 review_data.py --check
 
 .venv/bin/python3 download_data.py --clean_data --skip_download
 
 find data -type f -exec chmod -x {} +
 
-.venv/bin/python review_data.py meta data >review.$(date +%Y%m%d_%H%M%S).$(git rev-parse HEAD).$(git status --porcelain | grep -v '??' | wc -l).txt
+.venv/bin/python3 review_data.py meta data >review.$(date +%Y%m%d_%H%M%S).$(git rev-parse HEAD).$(git status --porcelain | grep -v '??' | wc -l).txt
 
-.venv/bin/python -m benchmark --scanner credsweeper --load .ci/empty_report.json | tee .ci/benchmark.txt
+.venv/bin/python3 -m benchmark --scanner credsweeper --load .ci/empty_report.json | tee .ci/benchmark.txt
